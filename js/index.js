1c52449dae5fa63b29862f56a866ce3afb1ae263
erzengelar <cd_la@msn.com>
2025-12-03 08:01:22 -0300

    Se adiciona JS
    - Se agregar link a carpeta js archivo index.js

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// Hola Mundo
+// console.log("Hola Mundo!");
+
+// Variables
+
+let num1 = 4;
+let num2 = 5;
+
+let incremento = num1++;
+let decremento = num1--;
+let resultadoSuma = num1 + num2;
+let resultadoResta = num1 - num2;
+let resultadoDiv = num1 / num2;
+let resultadoMult = num1 * num2;
+let resultadoMod = num1 % num2;
+
+console.log("incremento "+incremento);
+console.log("decremento "+decremento);
+console.log("Suma "+resultadoSuma);
+console.log("Resta "+resultadoResta);
+console.log("Division "+resultadoDiv);
+console.log("Multiplicación "+resultadoMult);
+console.log("Modulo "+resultadoMod);
